3b39bfc419dea264492d3aa99bff6189daacb0a9
RavenAcuzar <ricoraven28@gmail.com>
2021-05-18 12:52:37 +0800

    FIX (IOS): Create blurred screen on capture change

--- src/ios/PrivacyScreenPlugin.m
+++ src/ios/PrivacyScreenPlugin.m
@@ -51,14 +51,11 @@
     }
 
     NSLog(@"App is active");
-    BOOL isCaptured = [[UIScreen mainScreen] isCaptured];
     // grab a reference to our custom blur view
-   UIView *blurEffectView = [self.window viewWithTag:1234];
-    if (isCaptured) {
-        // fade in the view
-        [UIView animateWithDuration:0.5 animations:^{
-            blurEffectView.alpha = 1;
-        }];
+    UIView *blurEffectView = [self.window viewWithTag:1234];
+
+    if ([[UIScreen mainScreen] isCaptured]) {
+        NSLog(@"App is being captured");
     }
     else {
         // fade away colour view from main view
@@ -69,32 +66,32 @@
             [blurEffectView removeFromSuperview];
         }];
     }
-    // grab a reference to our custom blur view
-   // UIView *blurEffectView = [self.window viewWithTag:1234];
-
-    // fade away colour view from main view
-   // [UIView animateWithDuration:0.5 animations:^{
-    //    blurEffectView.alpha = 0;
-  //  } completion:^(BOOL finished) {
-        // remove when finished fading
-     //   [blurEffectView removeFromSuperview];
-   // }];
 }
 -(void)handleScreenCaptureChange
 {
-    // grab a reference to our custom blur view
-    UIView *blurEffectView = [self.window viewWithTag:1234];
-
     if (@available(iOS 11.0, *)) {
-        BOOL isCaptured = [[UIScreen mainScreen] isCaptured];
-        if (isCaptured) {
-    //Screen being captured"
-    // fade in the view
-    [UIView animateWithDuration:0.5 animations:^{
-        blurEffectView.alpha = 1;
-    }];
-        }
-          else{
+        if ([[UIScreen mainScreen] isCaptured]) {
+            NSLog(@"Capture Change: App is being captured");
+            //Screen being captured
+            self.window.backgroundColor = [UIColor clearColor];
+
+            UIBlurEffect *blurEffect = [UIBlurEffect effectWithStyle:UIBlurEffectStyleExtraLight];
+            UIVisualEffectView *blurEffectView = [[UIVisualEffectView alloc] initWithEffect:blurEffect];
+            blurEffectView.frame = self.window.bounds;
+            blurEffectView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+
+            blurEffectView.tag = 1234;
+            blurEffectView.alpha = 0;
+            [self.window addSubview:blurEffectView];
+            [self.window bringSubviewToFront:blurEffectView];
+
+            // fade in the view
+            [UIView animateWithDuration:0.5 animations:^{
+                blurEffectView.alpha = 1;
+            }];
+        } else{
+            NSLog(@"Capture Change: App is not being captured");
+            UIView *blurEffectView = [self.window viewWithTag:1234];
             // fade away colour view from main view
             [UIView animateWithDuration:0.5 animations:^{
                 blurEffectView.alpha = 0;
